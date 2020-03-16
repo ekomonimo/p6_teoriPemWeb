80a92ed0e53b47b0ad63bceff2ea0c10cf041be4
eko <ekomonimo@gmail.com>
2020-03-16 09:34:55 +0700

    tugas pertemuan 6

---
+++ app/src/main/java/prasetyo/muhamad/aboutme_p6/MainActivity.kt
@@ -0,0 +1,12 @@
+package prasetyo.muhamad.aboutme_p6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
